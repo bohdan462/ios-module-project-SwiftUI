4fb0d2e25f4f0664d261e9ec50d4d1e8b1a353cc
bohdan462 <tech.bohdan@gmail.com>
2020-07-11 13:01:00 -0500

    Completed Creating and Combining Views

--- CreatingandCombiningViews/Creating and Combining Views/Creating and Combining Views/ContentView.swift
+++ CreatingandCombiningViews/Creating and Combining Views/Creating and Combining Views/ContentView.swift
@@ -10,18 +10,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-                .multilineTextAlignment(.leading)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .multilineTextAlignment(.leading)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                        
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
             }
+            .padding()
+            
+            Spacer()
         }
-    .padding()
     }
 }
 
